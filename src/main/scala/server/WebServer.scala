0ce415cab419c68024915b34de902674d483033b
Fredrik Meyer <hrmeyer@gmail.com>
2018-02-11 21:44:52 +0100

    Make it work

--- src/main/scala/server/WebServer.scala
+++ src/main/scala/server/WebServer.scala
@@ -5,23 +5,27 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+
 import scala.io.StdIn
 
-class WebServer extends App {
+object WebServer extends App {
 
   implicit val system = ActorSystem("my-system")
   implicit val materializer = ActorMaterializer()
-  // needed for the future flatMap/onComplete in the end
+
   implicit val executionContext = system.dispatcher
 
   val route =
-    path("hello") {
+    path("") {
       get {
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Her kommer algebra :)</h1>"))
       }
     }
 
-  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+  val config = ConfigFactory.load()
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", config.getInt("http.port"))
 
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
